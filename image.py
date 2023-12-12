851058bfa435f0618d29689bb2a83cf50a10eb39
Tyler Ryan <tyler@Tylers-MacBook-Air.local>
2023-12-11 21:47:36 -0800

    Push image

--- image.py
+++ image.py
@@ -1,19 +1,22 @@
 import pulumi
 import pulumi_docker as docker
-#from pulumi_awsx import ecr
 
-#TODO: Add arguments
+# from pulumi_awsx import ecr
+
+
+# TODO: Add arguments
 # Containerized app component resource
 class MyImage(pulumi.ComponentResource):
-    def __init__(self, name, opts = None):
-        super().__init__('pkg:index:MyImage', name, None, opts)
+    def __init__(self, name, opts=None):
+        super().__init__("pkg:index:MyImage", name, None, opts)
 
-#TODO: create ECR repo resource
-#repository = ecr.Repository()
+    # TODO: create ECR repo resource
+    # repository = ecr.Repository()
 
-# Build image
-#TODO: make image update dynamic, switch from docker hub to ecr
-    hello_world = docker.Image("hello_world",
+    # Build image
+    # TODO: make image update dynamic, switch from docker hub to ecr
+    hello_world = docker.Image(
+        "hello_world",
         build=docker.DockerBuildArgs(
             args={
                 "platform": "linux/amd64",
@@ -22,8 +25,9 @@ class MyImage(pulumi.ComponentResource):
             dockerfile="Dockerfile",
         ),
         image_name="docker.io/tryan225/hello_world:v0.1.0",
-        skip_push=True)
+    )
     pulumi.export("imageName", hello_world.image_name)
     pulumi.export("repoDigest", hello_world.repo_digest)
 
-#TODO: publish to ECR?
+
+# TODO: publish to ECR?
